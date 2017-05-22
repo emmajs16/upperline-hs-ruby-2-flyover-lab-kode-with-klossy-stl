e5372a4298339fe7a3add1acaf2d0e03ce5b8ec0
Emma Stoverink <emmajs16@live.com>
2017-05-22 14:13:17 +0000

    Done.

--- flyover.rb
+++ flyover.rb
@@ -1,1 +1,12 @@
 ## Code your solution here.
+def pennsylvania
+  return "We're flying over pretty Pennsylvania!"
+end
+
+def west_virginia
+  return "We're flying over wonderful West Virginia!"
+end
+
+def indiana
+  return "We're flying over interesting Indiana!"
+end
